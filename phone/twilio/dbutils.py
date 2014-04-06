780f58bd177746f17ba5fc8391e97db606e82660
Chris Davies <mcd@daviesinc.com>
2014-04-06 07:11:28 +0000

    mailbox creation working

--- phone/twilio/dbutils.py
+++ phone/twilio/dbutils.py
@@ -4,3 +4,9 @@ from phone.models import (DBSession,
 def get_mailbox_url(mailbox_number):
     return DBSession.query(MailboxGreeting) \
         .filter(MailboxGreeting.mailbox==mailbox_number).first() 
+
+def create_mailbox(mailbox_number):
+    print 'creating mailbox', mailbox_number
+    mailbox = MailboxGreeting(mailbox=mailbox_number)
+    DBSession.add(mailbox)
+    DBSession.flush()
