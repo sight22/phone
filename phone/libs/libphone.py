62795806b9ede6e1051927939565b3044bacf1a2
Qwait <matthew@matthewhousden.com>
2014-04-06 14:57:59 +0000

    send mail admin stub

--- phone/libs/libphone.py
+++ phone/libs/libphone.py
@@ -5,6 +5,7 @@ from temboo.core.session import TembooSession
 from temboo.Library.Google.Geocoding import GeocodeByAddress
 from temboo.Library.Twilio.AvailablePhoneNumbers import LocalList
 from temboo.Library.Twilio.IncomingPhoneNumbers import AddPhoneNumber
+from temboo.Library.SendGrid.WebAPI.Mail import SendMail
 
 def get_phones(addr1, city, state, zip):
     config = ConfigParser.ConfigParser()
@@ -55,6 +56,27 @@ def buy_number(account_sid, number):
 
     addPhoneNumberResults = addPhoneNumberChoreo.execute_with_results(addPhoneNumberInputs)
 
-    print addPhoneNumberResults
+    #print addPhoneNumberResults
 
     return addPhoneNumberResults
+
+def send_mail(subject, data, to_address, from_address=None):
+    config = ConfigParser.ConfigParser()
+    config.readfp(open(os.path.join('/', os.path.dirname(os.path.dirname(
+        os.path.dirname(os.path.join(os.path.abspath(__file__))))), 'phone.cfg')))
+
+    session = TembooSession('cd34', config.get('temboo', 'APP_KEY_NAME'),
+        config.get('temboo', 'APP_KEY_VALUE'))
+
+    sendMailChoreo = SendMail(session)
+    sendMailInputs = sendMailChoreo.new_input_set()
+
+    if from_address is None:
+        from_address = config.get('sendgrid', 'default_from')
+
+    sendMailInputs.set_APIUser(config.get('sendgrid', 'api_user'))
+    sendMailInputs.set_APIKey(config.get('sendgrid', 'api_key'))
+    sendMailInputs.set_Subject(subject)
+    sendMailInputs.set_To(to_address)
+    sendMailInputs.set_From(from_address)
+    sendMailInputs.set_Text(data)
